7aab7844feda152e3bc06f14b82f0024cbb647e4
Talitha Medeiros <medeirostalitha@gmail.com>
2018-06-29 20:30:35 -0300

    Update trends.py

--- trends.py
+++ trends.py
@@ -2,10 +2,10 @@ import twitter
 
 class TwitterApi():
     def __init__(self):
-        self.api = twitter.Api(consumer_key='',
-                        consumer_secret='',
-                        access_token_key='',
-                        access_token_secret='')
+        self.api = twitter.Api(consumer_key='your_consumer_key',
+                               consumer_secret='your_consumer_secret',
+                               access_token_key='your_access_token_key',
+                               access_token_secret='yout_access_token_secret')
 
     def getTrends(self):
         response = self.api.GetTrendsCurrent()
